648a690038ee7dd7afdec6923048460e1de84075
Hp <Hp@DESKTOP-72PGT4P>
2023-06-17 01:54:29 -0500

    first commit

---
+++ src/main/java/com/evc1/diazmont/demo/MascotaRepository.java
@@ -0,0 +1,9 @@
+package com.evc1.diazmont.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MascotaRepository extends CrudRepository <Mascota,Integer> {
+
+    
+    
+}
